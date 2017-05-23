3389822b9922fe1974aa676478c1a2c49e2a2924
Lionel Miller <delonorm@gmail.com>
2017-05-24 02:04:53 +0300

    Make ast() handle more types

--- cohomology.py
+++ cohomology.py
@@ -24,25 +24,34 @@ def ast(expr, r):
     elif isinstance(expr, sp.Add):
         terms = [ast(term, r) for term in expr.args]
         return sum_terms(terms)
-    else:
+    elif isinstance(expr, sp.Mul):
         commutative, other = split_commutative(expr)
         commutative = sp.Mul(*commutative)
         other = sp.Mul(*other)
         if commutative == 1:
-            raise TypeError("Unknown type {} for expression {}".format(type(expr), expr))
+            raise TypeError("Don't know how to handle this product: {}".format(expr))
         else:
             result = ast(other, r)
             return multiply_termwise(commutative, result)
-
+    elif isinstance(expr, sp.Sum):
+        new_function = ast(expr.function, r)
+        if isinstance(new_function, sp.SeqFormula):
+            return sp.SeqFormula(sp.Sum(new_function.formula, expr.limits[0]), new_function.args[1])
+        else:
+            return sp.Sum(new_function, expr.limits[0])
+    else:
+        raise TypeError("Unknown type {} for expression {}".format(type(expr), expr))
 
 def ast_table(expr):
+    expr_expanded = texpand(expr)
     for r in basis:
-        product = ast(expr, r)
+        product = ast(expr_expanded, r)
         product = texpand(product)
         print('({}) ∗ {}\n    = {}'.format(expr, r, product))
 
 
 def ast_matrix(expr):
+    expr_expanded = texpand(expr)
     col1 = basis
-    col2 = [texpand(ast(expr, r)) for r in basis]
+    col2 = [texpand(ast(expr_expanded, r)) for r in basis]
     return sp.Matrix([col1, col2]).T
